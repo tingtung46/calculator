d93337c547186fe6d173eb80f8cf4c59babe3b05
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2023-11-09 20:50:01 +0700

    Add conditon if user input numbers and operator

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-function add(a, b) {
+function add(firstOperand, secondOperand) {
     return a + b;
 };
 
-function subtract(a, b) {
+function subtract(firstOperand, secondOperand) {
     return a - b;
 };
 
-function multiply(a, b) {
+function multiply(firstOperand, secondOperand) {
     return a * b;
 };
 
-function divide(a, b) {
+function divide(firstOperand, secondOperand) {
     return a / b;
 };
+
+let firstOperand = Number(1);
+let secondOperand = Number(1);
+let operator = '+';
+
+switch(operator) {
+    case '+':
+        console.log(add(firstOperand, secondOperand));
+        break;
+    case '-':
+        console.log(subtract(firstOperand, secondOperand));
+        break;
+    case 'x':
+        console.log(multiply(firstOperand, secondOperand));
+        break;
+    case ':':
+        console.log(divide(firstOperand, secondOperand));
+        break;
+}
